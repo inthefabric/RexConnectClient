ed5db17014afe0f75a0538e52ecf09899a81da60
Zach Kinstner <zach@aestheticinteractive.com>
2013-06-26 16:33:33 -0400

    Added new tests.
    All 59 tests pass.

---
+++ Solution/RexConnectClient.Test/RcCore/TRexConnContext.cs
@@ -0,0 +1,63 @@
+﻿using NUnit.Framework;
+using RexConnectClient.Core;
+using RexConnectClient.Core.Result;
+using RexConnectClient.Core.Transfer;
+
+namespace RexConnectClient.Test.RcCore {
+
+	/*================================================================================================*/
+	[TestFixture]
+	public class TRexConnContext : TestBase {
+
+
+		////////////////////////////////////////////////////////////////////////////////////////////////
+		/*--------------------------------------------------------------------------------------------*/
+		[Test]
+		public void New() {
+			var r = new Request();
+			const string host = "local";
+			const int port = 1234;
+
+			var ctx = new RexConnContext(r, host, port);
+
+			Assert.AreEqual(r, ctx.Request, "Incorrect Request.");
+			Assert.AreEqual(host, ctx.HostName, "Incorrect HostName.");
+			Assert.AreEqual(port, ctx.Port, "Incorrect Port.");
+		}
+
+		/*--------------------------------------------------------------------------------------------*/
+		[Test]
+		public void CreateResponseResult() {
+			var r = new Request();
+			var ctx = new RexConnContext(r, null, 0);
+			IResponseResult rr = ctx.CreateResponseResult();
+
+			Assert.NotNull(rr, "Result should not be null.");
+			Assert.AreEqual(ctx, rr.Context, "Incorrect result Context.");
+			Assert.AreEqual(r, rr.Request, "Incorrect result Request.");
+		}
+
+		/*--------------------------------------------------------------------------------------------*/
+		[Test]
+		[Category(Integration)]
+		public void CreateTcpClient() {
+			var r = new Request();
+			var ctx = new RexConnContext(r, "rexster", 8185);
+			IRexConnTcp tcp = ctx.CreateTcpClient();
+
+			Assert.NotNull(tcp, "Result should not be null.");
+			Assert.AreEqual(1<<16, tcp.SendBufferSize, "Incorrect result SendBufferSize.");
+			Assert.AreEqual(1<<16, tcp.ReceiveBufferSize, "Incorrect result ReceiveBufferSize.");
+		}
+
+		/*--------------------------------------------------------------------------------------------*/
+		[Test]
+		public void Log() {
+			var r = new Request();
+			var ctx = new RexConnContext(r, null, 0);
+			ctx.Log("Type", "Category", "Text");
+		}
+		
+	}
+
+}
---
+++ Solution/RexConnectClient.Test/RcCore/TRexConnTcp.cs
@@ -0,0 +1,55 @@
+﻿using NUnit.Framework;
+using RexConnectClient.Core;
+
+namespace RexConnectClient.Test.RcCore {
+
+	/*================================================================================================*/
+	[TestFixture]
+	public class TRexConnTcp : TestBase {
+
+
+		////////////////////////////////////////////////////////////////////////////////////////////////
+		/*--------------------------------------------------------------------------------------------*/
+		[Test]
+		[Category(Integration)]
+		public void New() {
+			BuildTcp();
+			Assert.Pass("Something failed.");
+		}
+
+		/*--------------------------------------------------------------------------------------------*/
+		[Test]
+		[Category(Integration)]
+		public void SendBufferSize() {
+			RexConnTcp tcp = BuildTcp();
+			tcp.SendBufferSize = 1234;
+			Assert.AreEqual(1234, tcp.SendBufferSize, "Incorrect result.");
+		}
+
+		/*--------------------------------------------------------------------------------------------*/
+		[Test]
+		[Category(Integration)]
+		public void ReceiveBufferSize() {
+			RexConnTcp tcp = BuildTcp();
+			tcp.ReceiveBufferSize = 1234;
+			Assert.AreEqual(1234, tcp.ReceiveBufferSize, "Incorrect result.");
+		}
+
+		/*--------------------------------------------------------------------------------------------*/
+		[Test]
+		[Category(Integration)]
+		public void GetStream() {
+			RexConnTcp tcp = BuildTcp();
+			Assert.NotNull(tcp.GetStream(), "Result should not be null.");
+		}
+
+
+		////////////////////////////////////////////////////////////////////////////////////////////////
+		/*--------------------------------------------------------------------------------------------*/
+		private RexConnTcp BuildTcp() {
+			return new RexConnTcp("rexster", 8185);
+		}
+		
+	}
+
+}
